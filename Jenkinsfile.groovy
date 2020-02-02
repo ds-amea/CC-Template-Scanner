8453b4645a35bf8f9846fb8b8dd8aad5294094ed
Mohamed Khaled (SE-EG) <mohamed_khaled@trendmicro.com>
2020-02-02 13:56:43 +0200

    Template Scanner

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,18 @@
+node {
+ 
+    cleanWs()
+    stage "Checkout Code"
+
+    sh "git clone https://github.com/ds-amea/CC-Template-Scanner.git"
+
+    stage "Scan"
+
+    sh "pip3 install -r https://raw.githubusercontent.com/OzNetNerd/Cloud-Conformity-Pipeline-Scanner/master/code/requirements.txt --user"
+    sh "wget https://raw.githubusercontent.com/OzNetNerd/Cloud-Conformity-Pipeline-Scanner/master/code/scanner.py"
+    sh "python3 scanner.py"
+
+    stage "Build the Environment"
+
+    sh "aws --region us-west-2 cloudformation create-stack --stack-name myteststackk --template-body file://$CFN_TEMPLATE_FILE_LOCATION --capabilities CAPABILITY_NAMED_IAM"
+}
+
